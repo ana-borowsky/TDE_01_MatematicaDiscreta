c3b12717373b30b5b3995ce9d3d7e39c46a65c16
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-03-14 09:04:42 -0300

    Adiciona leitura dos conjuntos diretamente do texto

--- TDE_01.py
+++ TDE_01.py
@@ -20,28 +20,34 @@ def executaOperacao():
         case "C":
             print("Produto Cartesiano do conjunto A e B")
             resultado = [(a, b) for a in conjuntoA for b in conjuntoB]
-            print(resultado)
+            print(set(resultado))
         case "D":
             print("Diferença do conjunto A e B")
             resultado = set(conjuntoA) - set(conjuntoB)
             print(resultado)
         case _: 
-            print("Opção desconhecida.")
+            print("Opção desconhecida.")
 
-j = 1
 i = 0
-conjuntoA = [3, 5, 67, 7]
-conjuntoB = [1, 2, 3, 4]
+j = 1
+k = 2
+l = 3
 
 with open('teste1.txt', 'r') as arquivo:
     linhas = arquivo.readlines()
     numero_operacoes = int(linhas[0])
     operacao = linhas[j].strip()
+    conjuntoA = linhas[k].strip().split(',')
+    conjuntoB = linhas[l].strip().split(',')
 
 while i < numero_operacoes:
     executaOperacao()
     j += 3
+    k += 3
+    l += 3
     while j < numero_operacoes * 3:
         operacao = linhas[j].strip()
+        conjuntoA = linhas[k].strip().split(',')
+        conjuntoB = linhas[l].strip().split(',')
         break
     i += 1
